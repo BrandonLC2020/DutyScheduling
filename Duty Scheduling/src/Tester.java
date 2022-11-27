7e9a19e4ce2020ee31ad06a32cf271d77eb7f578
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2022-11-26 20:29:28 -0500

    more and more work

--- Duty Scheduling/src/Tester.java
+++ Duty Scheduling/src/Tester.java
@@ -1,10 +1,14 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 
 public class Tester {
     public static void main(String[] args) {
         Analyzer testAnalyzer = new Analyzer();
         MonthOfDuty january = new MonthOfDuty("January", 2023);
+        MonthOfDuty february = new MonthOfDuty("February", 2023);
+        MonthOfDuty march = new MonthOfDuty("March", 2023);
+        MonthOfDuty april = new MonthOfDuty("April", 2023);
         MonthOfDuty may = new MonthOfDuty("May", 2023);
         RA brandon = new RA("Brandon", DayOfWeek.MONDAY);
         RA sanjana = new RA("Sanjana", DayOfWeek.WEDNESDAY);
